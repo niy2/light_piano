497599e5dcacff7cbda7ec540ba0ce1d57e67648
niy2 <niy2@miamioh.edu>
2017-03-16 01:10:06 -0400

    Add files via upload

---
+++ frequency.ino
@@ -0,0 +1,24 @@
+//********************revised borrowed code*********************
+#define Cd 262
+#define CSd 277
+#define Dd 294
+#define DSd 311
+#define Ed 330
+#define Fd 349
+#define FSd 370
+#define Gd 392
+#define GSd 415
+#define Ad 440
+#define ASd 466
+#define Bd 494
+#define Cf 523
+#define CSf 554
+#define Df 587
+#define DSf 622
+#define Ef 659
+//The original code was using C4-E5. However, this cause my sensors
+//don't work properly. Not sure why...
+//Author:gunpole
+//Source：guokr
+//Link：http://www.guokr.com/article/7848/
+//***************************************************************
